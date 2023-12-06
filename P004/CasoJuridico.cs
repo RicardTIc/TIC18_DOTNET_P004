2f7244b7a33e333fbd4232eca4a12bd12103d3f0
EzekLobo <eloliveira.cic@uesc.br>
2023-12-06 19:35:42 -0300

    Implementação de novos metodos

--- P004/CasoJuridico.cs
+++ P004/CasoJuridico.cs
@@ -10,6 +10,67 @@ public class CasoJuridico
     public string Status { get; set; }
     public List<Advogado> Advogados { get; set; }
     public Cliente Cliente { get; set; }
+     public void IniciarCaso()
+    {
+        if (Status == "Em aberto")
+        {
+            Abertura = DateTime.Now;
+            Status = "Em andamento";
+            Console.WriteLine("Caso iniciado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("O caso já está em andamento ou foi concluído/arquivado.");
+        }
+    }
+
+    public void AtualizarCaso(float probabilidadeSucesso, List<(float, string)> custos)
+    {
+        if (Status == "Em andamento")
+        {
+            ProbabilidadeSucesso = probabilidadeSucesso;
+            Custos = custos;
+            Console.WriteLine("Caso atualizado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("O caso não está em andamento.");
+        }
+    }
+
+    public void ConcluirCaso(DateTime dataConclusao)
+    {
+        if (Status == "Em andamento")
+        {
+            if (dataConclusao > Abertura)
+            {
+                Encerramento = dataConclusao;
+                Status = "Concluído";
+                Console.WriteLine("Caso concluído com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("A data de conclusão deve ser posterior à data de início.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("O caso não está em andamento.");
+        }
+    }
+
+    public void ArquivarCaso()
+    {
+        if (Status == "Concluído")
+        {
+            Status = "Arquivado";
+            Console.WriteLine("Caso arquivado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("O caso não está concluído.");
+        }
+    }
 
     public CasoJuridico()
     {   
--- P004/EscritorioAdvocacia.cs
+++ P004/EscritorioAdvocacia.cs
@@ -20,13 +20,13 @@ public class EscritorioAdvocacia
     private List<Advogado> advogados = new List<Advogado>();
     private List<Cliente> clientes = new List<Cliente>();
 
-     public void InserirCasoJuridico(CasoJuridico casoJuridico)
+    public void InserirCasoJuridico(CasoJuridico casoJuridico)
     {
         casoJuridicoManagement.AddEntity(casoJuridico);
         Console.WriteLine($"Caso Jurídico aberto para o cliente {casoJuridico.Cliente.Nome} inserido com sucesso.");
     }
 
-       public void RemoverCasoJuridico(CasoJuridico casoJuridico)
+    public void RemoverCasoJuridico(CasoJuridico casoJuridico)
     {
         casoJuridicoManagement.RemoveEntity(casoJuridico);
         Console.WriteLine($"Caso Jurídico para o cliente {casoJuridico.Cliente.Nome} removido com sucesso.");
@@ -34,7 +34,7 @@ public class EscritorioAdvocacia
 
 
 
-     public void AdicionarAdvogado(Advogado advogado)
+    public void AdicionarAdvogado(Advogado advogado)
     {
         try
         {
@@ -56,8 +56,8 @@ public class EscritorioAdvocacia
         }
     }
 
-    
-     public void AdicionarCliente(Cliente cliente)
+
+    public void AdicionarCliente(Cliente cliente)
     {
         try
         {
@@ -78,42 +78,48 @@ public class EscritorioAdvocacia
             Console.WriteLine($"Erro desconhecido ao adicionar cliente: {ex.Message}");
         }
     }
-      
-       public IEnumerable<Advogado> ObterAdvogadosPorIdade(int idadeMinima, int idadeMaxima)
+
+    public IEnumerable<Advogado> ObterAdvogadosPorIdade(int idadeMinima, int idadeMaxima)
     {
         return advogados.Where(a => CalcularIdade(a.DataNascimento) >= idadeMinima && CalcularIdade(a.DataNascimento) <= idadeMaxima);
     }
+
 
     public IEnumerable<Cliente> ObterClientesPorIdade(int idadeMinima, int idadeMaxima)
     {
         return clientes.Where(c => CalcularIdade(c.DataNascimento) >= idadeMinima && CalcularIdade(c.DataNascimento) <= idadeMaxima);
     }
 
+
     public IEnumerable<Cliente> ObterClientesPorEstadoCivil(EstadoCivil estadoCivil)
     {
         return clientes.Where(c => c.EstadoCivil == estadoCivil);
     }
 
+
     public IEnumerable<Cliente> ObterClientesOrdemAlfabetica()
     {
         return clientes.OrderBy(cliente => cliente.Nome);
     }
-    
 
-     public IEnumerable<Cliente> ObterClientesPorProfissao(string textoProfissao)
+
+
+    public IEnumerable<Cliente> ObterClientesPorProfissao(string textoProfissao)
     {
         return clientes.Where(cliente => cliente.Profissao.Contains(textoProfissao, StringComparison.OrdinalIgnoreCase));
     }
-    public IEnumerable<Pessoa> ObterAniversariantesDoMes(int mes)
-{
-    var aniversariantes = advogados
-        .Select(a => (Pessoa)a) // Converte Advogado para Pessoa
-        .Concat(clientes)
-        .Where(pessoa => pessoa.DataNascimento.Month == mes)
-        .OrderBy(pessoa => pessoa.DataNascimento.Day);
 
-    return aniversariantes;
-}
+    public IEnumerable<Pessoa> ObterAniversariantesDoMes(int mes)
+    {
+        var aniversariantes = advogados
+            .Select(a => (Pessoa)a) // Converte Advogado para Pessoa
+            .Concat(clientes)
+            .Where(pessoa => pessoa.DataNascimento.Month == mes)
+            .OrderBy(pessoa => pessoa.DataNascimento.Day);
+
+        return aniversariantes;
+    }
+
 
 
 
@@ -129,4 +135,57 @@ public class EscritorioAdvocacia
 
         return idade;
     }
+
+    public IEnumerable<CasoJuridico> ObterCasosEmAberto()
+    {
+        return casoJuridicoManagement.GetAllEntities().Where(c => c.Status == "Em aberto").OrderBy(c => c.Abertura);
+    }
+    public IEnumerable<Advogado> ObterAdvogadosPorQuantidadeCasosConcluidos()
+    {
+        return advogadoManagement.GetAllEntities().OrderByDescending(a => a.CasosJuridicos.Count(c => c.Status == "Concluído"));
+    }
+    public IEnumerable<CasoJuridico> ObterCasosPorDescricaoCusto(string palavraChave)
+    {
+        return casoJuridicoManagement.GetAllEntities().Where(c => c.Custos.Any(cost => cost.Item2.Contains(palavraChave, StringComparison.OrdinalIgnoreCase)));
+    }
+    public IEnumerable<string> ObterTop10TiposDocumentosMaisInseridos()
+    {
+        var tiposDocumentos = casoJuridicoManagement.GetAllEntities()
+            .SelectMany(c => c.Documentos.Select(d => d.Tipo))
+            .GroupBy(tipo => tipo)
+            .OrderByDescending(group => group.Count())
+            .Take(10)
+            .Select(group => group.Key);
+
+        return tiposDocumentos;
+    }
+
+    public void ListarAdvogadosPorIdade()
+    {
+        Console.Write("Informe a idade mínima: ");
+        if (int.TryParse(Console.ReadLine(), out int idadeMinima))
+        {
+            Console.Write("Informe a idade máxima: ");
+            if (int.TryParse(Console.ReadLine(), out int idadeMaxima))
+            {
+                var advogadosPorIdade = ObterAdvogadosPorIdade(idadeMinima, idadeMaxima);
+
+                Console.WriteLine("\nAdvogados com idade entre {0} e {1} anos:\n", idadeMinima, idadeMaxima);
+
+                foreach (var advogado in advogadosPorIdade)
+                {
+                    Console.WriteLine($"Nome: {advogado.Nome}, Idade: {CalcularIdade(advogado.DataNascimento)} anos, CPF: {advogado.CPF}, CNA: {advogado.CNA}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Idade máxima inválida.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Idade mínima inválida.");
+        }
+    }
+
 }
