5ab9fef4520918ee7a83d1c49e0ee9495149d43f
Ricardo <cat-richard@hotmail.com>
2023-12-06 16:31:38 -0300

    parte 2

---
+++ Semana 04/Cliente.cs
@@ -0,0 +1,8 @@
+public class Cliente
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+    public string EstadoCivil { get; set; }
+    public string Profissao { get; set; }
+}
