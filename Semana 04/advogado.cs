e5edc823c588a09cae00ce7adcd1628e5e75f87f
Ricardo <cat-richard@hotmail.com>
2023-12-06 11:53:48 -0300

    class advogado

---
+++ Semana 04/advogado.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Advogado
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+    public string CNA { get; set; }
+}
