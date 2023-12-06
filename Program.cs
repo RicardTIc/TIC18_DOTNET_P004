d92f16d62445c7f6b5976ac36d5a613b484dbbee
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-06 19:23:58 -0300

    Adicionando classe Cliente

--- Program.cs
+++ Program.cs
@@ -22,3 +22,16 @@ class Advogado : Pessoa
         CNA = cna;
     }
 }
+
+class Cliente : Pessoa
+{
+    public string EstadoCivil { get; set; }
+    public string Profissao { get; set; }
+
+    public Cliente(string nome, DateTime dataNascimento, string cpf, string estadoCivil, string profissao)
+        : base(nome, dataNascimento, cpf)
+    {
+        EstadoCivil = estadoCivil;
+        Profissao = profissao;
+    }
+}
