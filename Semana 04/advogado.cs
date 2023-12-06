cd1b679afce6f2f97da28e07b0c10a53b822f575
Ricardo <cat-richard@hotmail.com>
2023-12-06 15:36:24 -0300

    class cliente

--- Semana 04/advogado.cs
+++ Semana 04/advogado.cs
@@ -9,3 +9,13 @@ public class Advogado
     public string CPF { get; set; }
     public string CNA { get; set; }
 }
+
+public class Cliente
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+    public string EstadoCivil { get; set; }
+    public string Profissao { get; set; }
+}
+
