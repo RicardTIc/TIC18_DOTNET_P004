1957e4f736918f8cfd60274b9baea6c35957a058
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-06 19:23:03 -0300

    Adicionando classe abstrata Pessoa

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Pessoa
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+
+    public Pessoa(string nome, DateTime dataNascimento, string cpf)
+    {
+        Nome = nome;
+        DataNascimento = dataNascimento;
+        CPF = cpf;
+    }
+}
