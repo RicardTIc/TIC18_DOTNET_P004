790fc4188db832d3a30de8c0c281523bb5bf4bd3
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-06 19:23:38 -0300

    Adicionando classe Advogado

--- Program.cs
+++ Program.cs
@@ -11,3 +11,14 @@
         CPF = cpf;
     }
 }
+
+class Advogado : Pessoa
+{
+    public string CNA { get; set; }
+
+    public Advogado(string nome, DateTime dataNascimento, string cpf, string cna)
+        : base(nome, dataNascimento, cpf)
+    {
+        CNA = cna;
+    }
+}
