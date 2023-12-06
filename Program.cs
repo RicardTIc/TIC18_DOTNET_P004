8d1428c7baeb9889f77a863d3589d922a257b52d
Ian Rodrigues Alexandrino <133264089+IanAlexandrino@users.noreply.github.com>
2023-12-06 19:44:42 -0300

    Adicionando classe CasoJuridico

--- Program.cs
+++ Program.cs
@@ -43,3 +43,22 @@ class Documento
     public string Tipo { get; set; }
     public string Descricao { get; set; }
 }
+
+class CasoJuridico
+{
+    public DateTime Abertura { get; set; }
+    public float ProbabilidadeSucesso { get; set; }
+    public List<Documento> Documentos { get; set; }
+    public List<float> Custos { get; set; }
+    public DateTime? Encerramento { get; set; }
+    public List<Advogado> Advogados { get; set; }
+    public Cliente Cliente { get; set; }
+    public string Status { get; set; }
+
+    public CasoJuridico()
+    {
+        Documentos = new List<Documento>();
+        Custos = new List<float>();
+        Advogados = new List<Advogado>();
+    }
+}
