1a51b27ec5f90f4bf74534183bb0c0ecd85528f1
EzekLobo <eloliveira.cic@uesc.br>
2023-12-07 15:27:24 -0300

    Novas classes

---
+++ AV-T5/Solucao/Classes/IPagamento.cs
@@ -0,0 +1,8 @@
+ public interface IPagamento
+    {
+        string Tipo { get; set; }
+        string Descricao { get; set; }
+        double ValorBruto { get; set; }
+        double Desconto { get; set; }
+        DateTime DataHora { get; set; }
+    }
