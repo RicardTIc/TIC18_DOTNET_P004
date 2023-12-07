bbfefcd05272f330d9538f8b6ad9111fc7903cda
EzekLobo <eloliveira.cic@uesc.br>
2023-12-07 16:21:15 -0300

    Modificações

--- AV-T5/Solucao/Classes/PlanoConsultoria.cs
+++ AV-T5/Solucao/Classes/PlanoConsultoria.cs
@@ -7,40 +7,50 @@ o Valor por mês (double)
 o Benefícios (List<string>)
 
 */
-public class PlanoConsultoria{
-    public string Titulo { get; set; }
-    public double ValorPorMes { get; set; }
-    public List<string> Beneficios { get; set; }
+ public class PlanoConsultoria
+    {
+        public string Titulo { get; set; }
+        public double ValorPorMes { get; set; }
+        public List<string> Beneficios { get; set; }
+        public List<IPagamento> Pagamentos { get; set; }
 
-    public PlanoConsultoria(string _titulo, double _valorPorMes, List<string> _beneficios){
-        this.Titulo = _titulo;
-        this.ValorPorMes = _valorPorMes;
-        this.Beneficios = _beneficios;
-    }
-
-    public override string ToString(){
-        return this.Titulo + " - " + this.ValorPorMes + " - " + this.Beneficios;
-    }
-
-    public override bool Equals(object obj){
-        if (obj == null)
-            return false;
-        if (obj is PlanoConsultoria){
-            PlanoConsultoria plano = (PlanoConsultoria)obj;
-            return this.Titulo == plano.Titulo && this.ValorPorMes == plano.ValorPorMes && this.Beneficios == plano.Beneficios;
+        public PlanoConsultoria(string _titulo, double _valorPorMes, List<string> _beneficios)
+        {
+            this.Titulo = _titulo;
+            this.ValorPorMes = _valorPorMes;
+            this.Beneficios = _beneficios;
+            this.Pagamentos = new List<IPagamento>();
         }
-        return false;
-    }
 
-    public override int GetHashCode(){
-        return base.GetHashCode();
-    }
+        public override string ToString()
+        {
+            return this.Titulo + " - " + this.ValorPorMes + " - " + string.Join(", ", this.Beneficios);
+        }
 
-    public static bool operator ==(PlanoConsultoria plano1, PlanoConsultoria plano2){
-        return plano1.Equals(plano2);
-    }
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (obj is PlanoConsultoria)
+            {
+                PlanoConsultoria plano = (PlanoConsultoria)obj;
+                return this.Titulo == plano.Titulo && this.ValorPorMes == plano.ValorPorMes && this.Beneficios.SequenceEqual(plano.Beneficios);
+            }
+            return false;
+        }
 
-    public static bool operator !=(PlanoConsultoria plano1, PlanoConsultoria plano2){
-        return !plano1.Equals(plano2);
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(PlanoConsultoria plano1, PlanoConsultoria plano2)
+        {
+            return plano1.Equals(plano2);
+        }
+
+        public static bool operator !=(PlanoConsultoria plano1, PlanoConsultoria plano2)
+        {
+            return !plano1.Equals(plano2);
+        }
     }
-}
